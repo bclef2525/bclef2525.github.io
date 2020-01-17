57e7b3c2d4c9462b9bc08f567c6917c82dd0c717
bclef2525 <bclef25@gmail.com>
2020-01-17 16:21:12 +0900

    Updates

---
+++ precache-manifest.af8042a973e1df5864724466429a06ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b15a560073fd34b99cab88218e36118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2e66f1f12c44a73d2f5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dc3478242b460178e9b5",
+    "url": "/static/js/2.db592565.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.db592565.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2e66f1f12c44a73d2f5",
+    "url": "/static/js/main.4868e6a2.chunk.js"
+  },
+  {
+    "revision": "917bb86a6f09c126c185",
+    "url": "/static/js/runtime-main.41da76da.js"
+  },
+  {
+    "revision": "6ee36e9f40dd47eb5a46f67afeee8a01",
+    "url": "/static/media/air_bnb_main.6ee36e9f.png"
+  }
+]);
