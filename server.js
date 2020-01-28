12d58fa781e547d49ab786cee606d8da003a6740
bclef2525 <bclef25@gmail.com>
2020-01-28 14:16:23 +0900

    express server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const env = process.env.ENV;
+const app = express();
+
+const isDevelopment = env.NODE_ENV === "development";
+
+app.use(logger(isDevelopment ? "dev" : "combined"));
+
+app.get("/ping", function(req, res) {
+  res.send("pong");
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function(req, res) {
+  res.set({
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Date: new Date()
+  });
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const port = isDevelopment ? 3000 : 80;
+
+app.listen(port, () => {
+  console.log("listening: " + env);
+});
